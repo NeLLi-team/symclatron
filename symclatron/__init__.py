465fc8a2db74fae717abfbf901960b65b9168f50
Juan C. Villada <jvillada@lbl.gov>
2025-08-25 15:38:15 -0700

    Bump version to 0.6.4 and update TensorFlow configuration in recipe.yaml

--- symclatron/__init__.py
+++ symclatron/__init__.py
@@ -17,7 +17,7 @@ Lawrence Berkeley National Laboratory (LBNL)
 """
 
 # Version information - required by flit for dynamic version
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 # Author information - required by flit
 __author__ = "Juan C. Villada"
--- symclatron/symclatron.py
+++ symclatron/symclatron.py
@@ -49,7 +49,7 @@ Lawrence Berkeley National Laboratory (LBNL)
 2025
 """
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 def version_callback(value: bool):
     """Print version information."""
