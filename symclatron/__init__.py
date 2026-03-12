eeb8ca103b8aaeaab2650d8d752266182fcc7069
Juan C. Villada <jvillada@lbl.gov>
2026-03-12 06:19:43 +0000

    Release 0.10.2

--- symclatron/__init__.py
+++ symclatron/__init__.py
@@ -17,7 +17,7 @@ Lawrence Berkeley National Laboratory (LBNL)
 """
 
 # Version information - required by flit for dynamic version
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 # Author information - required by flit
 __author__ = "Juan C. Villada"
